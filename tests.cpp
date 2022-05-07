d9f53812c13304bcc62380e016cd1a876bc8ad7e
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2022-05-07 19:37:57 -0400

    Task B tests

--- tests.cpp
+++ tests.cpp
@@ -8,3 +8,9 @@ TEST_CASE("printing range"){
   CHECK(printRange(1, 4) == "1 2 3 4 ");
   CHECK(printRange(-5, 0) == "-5 -4 -3 -2 -1 0 ");
 }
+
+TEST_CASE("summing range"){
+  CHECK(sumRange(4, 18) == 165);
+  CHECK(sumRange(-33, 53) == 870);
+  CHECK(sumRange(0, 0) == 0);
+}
